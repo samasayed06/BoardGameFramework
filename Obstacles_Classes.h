5c22d55120158b7834bee56b782cb639b9a64240
20240630_nada <20240630@stud.fci-cu.edu.eg>
2025-11-25 22:04:49 +0200

    i added smart option to games 10 and 3

--- Obstacles_Classes.h
+++ Obstacles_Classes.h
@@ -1,35 +1,3 @@
-/*#ifndef OBSTACLES_CLASSES_H
-#define OBSTACLES_CLASSES_H
-
-#include "BoardGame_Classes.h"
-
-class Obstacles_Board : public Board<char> {
-public:
-    Obstacles_Board();
-
-    bool update_board(Move<char>* move) override;
-    bool is_win(Player<char>* player) override;
-    bool is_lose(Player<char>* player) override;
-    bool is_draw(Player<char>* player) override;
-    bool game_is_over(Player<char>* player) override;
-
-private:
-    void add_random_obstacles(int count);
-    bool has_four_in_row(char sym) const;
-};
-
-class Obstacles_UI : public UI<char> {
-public:
-    Obstacles_UI();
-    Move<char>* get_move(Player<char>* player) override;
-};
-
-#endif*/ 
-
-
-
-
-
 #ifndef OBSTACLES_CLASSES_H
 #define OBSTACLES_CLASSES_H
 
--- TTT5_classes.h
+++ TTT5_classes.h
@@ -1,33 +1,3 @@
-/*#ifndef TTT5_CLASSES_H
-#define TTT5_CLASSES_H
-
-#include "BoardGame_Classes.h"
-
-class TTT5_Board : public Board<char> {
-public:
-    TTT5_Board();
-
-    bool update_board(Move<char>* move) override;
-    bool is_win(Player<char>* player) override;
-    bool is_lose(Player<char>* player) override;
-    bool is_draw(Player<char>* player) override;
-    bool game_is_over(Player<char>* player) override;
-
-private:
-    int count_sequences(char sym) const;
-};
-
-class TTT5_UI : public UI<char> {
-public:
-    TTT5_UI();
-
-    Move<char>* get_move(Player<char>* player) override;
-};
-
-#endif*/ 
-
-
-
 #ifndef TTT5_CLASSES_H
 #define TTT5_CLASSES_H
 
